767b1b44020aca09c33549c408af9a6075be36b7
PanosBouras <panosbouras@protonmail.com>
2026-04-23 21:43:15 +0300

    Remove ConnectionString from repo

--- OrderService/ConnectionString.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace OrderService
-{
-    public static class ConnectionString
-    {
-        public static string Value { get; set; } = $"Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=orcl)));User Id=system;Password=@m2%9WFy;";
-
-    }
-}
